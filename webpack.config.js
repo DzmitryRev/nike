befff06a15b2a2c34be848ada9e4502b946d942e
unknown <revchenkodzmitry@gmail.com>
2022-04-02 14:50:41 +0300

    feat: finish setup webpack | babel | postcss

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+let mode = "development";
+let target = "web";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  target = "browserslist";
+}
+
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: "[name].[contenthash].css",
+  }),
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+  }),
+];
+
+module.exports = {
+  mode,
+  target,
+  plugins,
+  devtool: "source-map",
+  entry: "./src/index.js",
+  devServer: {
+    hot: true,
+    static: {
+      directory: path.join(__dirname, "./dist"),
+    },
+    port: 9000,
+    compress: true,
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[name][ext][query]",
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      { test: /\.(html)$/, use: ["html-loader"] },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === "production" ? "asset" : "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+};
