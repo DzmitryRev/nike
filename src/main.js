d43261efa4d18b123b0656ca380624abbc90919e
unknown <revchenkodzmitry@gmail.com>
2022-04-02 18:15:23 +0300

    feat: finish project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./styles/index.scss";
+
+let burger = document.querySelector(".burger");
+let list = document.querySelector(".nav-list");
+
+burger.addEventListener("click", () => {
+  list.classList.toggle("open");
+  list.classList.contains("open")
+    ? burger.classList.add("rotated")
+    : burger.classList.remove("rotated");
+});
